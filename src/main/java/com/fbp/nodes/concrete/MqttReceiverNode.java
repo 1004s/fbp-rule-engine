d70f9515057d5280d021aaccb860d7870293b693
sylim <sammys27@naver.com>
2025-01-01 19:56:05 +0900

    [fix] Eclipse paho 라이브러리를 hivemq로 변경
    자바 버전과의 호환성 이슈 해결을 위함
    이슈 해결 방안 중 2번을 선택하였음
    resolves #5

--- src/main/java/com/fbp/nodes/concrete/MqttReceiverNode.java
+++ src/main/java/com/fbp/nodes/concrete/MqttReceiverNode.java
@@ -1,26 +1,34 @@
 package com.fbp.nodes.concrete;
 
+import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fbp.message.MqttDataMessage;
 import com.fbp.message.StatisticsMessage;
 import com.fbp.nodes.basic.InNode;
-import org.eclipse.paho.client.mqttv3.*;
+import com.hivemq.client.mqtt.datatypes.MqttQos;
+import com.hivemq.client.mqtt.mqtt3.Mqtt3AsyncClient;
+import com.hivemq.client.mqtt.mqtt3.Mqtt3Client;
+import com.hivemq.client.mqtt.mqtt3.message.connect.Mqtt3Connect;
+
+import java.util.Arrays;
 
 public class MqttReceiverNode extends InNode {
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
-    private final String brokerAddress; // tcp://192.168.70.203:1883
+    private final String brokerHost;
+    private final int brokerPort;
     private final String clientId;
     private final String[] topics;
     private JsonNode options;
-    private MqttClient mqttClient;
-    private MqttConnectOptions mqttConnectOptions;
+    private Mqtt3AsyncClient mqttClient;
 
-    public MqttReceiverNode(String id, String brokerAddress, String clientId, String[] topics,
+    public MqttReceiverNode(String id, String brokerHost, int brokerPort,
+                            String clientId, String[] topics,
                             JsonNode options) {
         super(id);
-        this.brokerAddress = brokerAddress;
+        this.brokerHost = brokerHost;
+        this.brokerPort = brokerPort;
         this.clientId = clientId;
         if (topics.length > 50) {
             StatisticsMessage errorMessage = new StatisticsMessage(getId(), StatisticsMessage.StatisticsDataType.ERROR,
@@ -30,113 +38,94 @@ public class MqttReceiverNode extends InNode {
         }
         this.topics = topics;
         this.options = options;
-        try {
-            mqttClient = new MqttClient(brokerAddress, clientId);
-        } catch (MqttException e) {
-            throw new RuntimeException(e);
-        }
-        mqttConnectOptions = new MqttConnectOptions();
     }
 
     @Override
     protected void initialize() {
-        setCustomConnectOptions();
-        mqttClient.setCallback(new MqttCallback() {
-            @Override
-            public void connectionLost(Throwable cause) {
-
-                // error log
-
-                StatisticsMessage errorMessage = new StatisticsMessage(getId(),
-                        StatisticsMessage.StatisticsDataType.ERROR, "Mqtt client connection lost!!");
-                addMessage(errorMessage);
-            }
-
-            @Override
-            public void messageArrived(String topic, MqttMessage message) throws Exception {
-                StatisticsMessage inputMessage = new StatisticsMessage(getId(),
-                        StatisticsMessage.StatisticsDataType.INPUT);
-                addMessage(inputMessage);
-
-                JsonNode payload = objectMapper.readTree(message.getPayload());
-                MqttDataMessage dataMessage = new MqttDataMessage(topic, payload);
-                addMessage(dataMessage);
-                StatisticsMessage outputMessage = new StatisticsMessage(getId(),
-                        StatisticsMessage.StatisticsDataType.OUTPUT);
-                addMessage(outputMessage);
-            }
-
-            @Override
-            public void deliveryComplete(IMqttDeliveryToken token) {
-
-            }
-        });
-
-        try {
-            mqttClient.connect(mqttConnectOptions);
-        } catch (MqttException e) {
-            StatisticsMessage errorMessage = new StatisticsMessage(getId(),
-                    StatisticsMessage.StatisticsDataType.ERROR, "Mqtt client connection failed!!");
-            addMessage(errorMessage);
-            throw new RuntimeException(e);
-        }
-
-        try {
-            mqttClient.subscribe(topics);
-        } catch (MqttException e) {
-            StatisticsMessage errorMessage = new StatisticsMessage(getId(),
-                    StatisticsMessage.StatisticsDataType.ERROR, "Mqtt client subscribe failed!!");
-            addMessage(errorMessage);
-            throw new RuntimeException(e);
-        }
+        mqttClient = Mqtt3Client.builder()
+                .identifier(clientId)
+                .serverHost(brokerHost)
+                .serverPort(brokerPort)
+                .buildAsync();
+        connectWithCustomConnectOptions();
     }
 
-    private void setCustomConnectOptions() {
+    private void connectWithCustomConnectOptions() {
+        boolean cleanSession = true;
+        int keepAlive = 60;
         if (options.get("cleanSession") != null && !options.get("cleanSession").booleanValue()) {
-            mqttConnectOptions.setCleanSession(false);
+            cleanSession = false;
         }
-        if (options.get("userName") != null) {
-            mqttConnectOptions.setUserName(options.get("userName").textValue());
-        }
-        if (options.get("password") != null) {
-            mqttConnectOptions.setPassword(options.get("password").textValue().toCharArray());
+        if (options.get("keepAliveInterval") != null) {
+            keepAlive = options.get("keepAliveInterval").intValue();
         }
         if (options.get("will") != null) {
             JsonNode will = options.get("will");
             try {
                 String topic = will.get("topic").textValue();
                 byte[] payload = will.get("payload").textValue().getBytes();
-                int qos = will.get("qos").intValue();
+                String qos = will.get("qos").textValue();
                 boolean retained = will.get("retained").booleanValue();
 
-                mqttConnectOptions.setWill(topic, payload, qos, retained);
+                mqttClient.connectWith()
+                        .cleanSession(cleanSession)
+                        .willPublish()
+                            .topic(topic)
+                            .payload(payload)
+                            .qos(MqttQos.valueOf(qos))
+                            .retain(retained).applyWillPublish()
+                        .keepAlive(keepAlive)
+                        .send();
             } catch (NullPointerException e) {
                 throw new IllegalArgumentException("설정파일 형식이 잘못되었습니다. topic, payload, qos, retained 모두 포함해야 함");
             }
-        }
-        if (options.get("connectionTimeout") != null) {
-            mqttConnectOptions.setConnectionTimeout(options.get("connectionTimeout").intValue());
-        }
-        if (options.get("keepAliveInterval") != null) {
-            mqttConnectOptions.setKeepAliveInterval(options.get("keepAliveInterval").intValue());
-        }
-        if (options.get("automaticReconnect") != null) {
-            mqttConnectOptions.setAutomaticReconnect(options.get("automaticReconnect").booleanValue());
+        } else {
+            mqttClient.connectWith()
+                    .cleanSession(cleanSession)
+                    .keepAlive(keepAlive)
+                    .send();
         }
     }
 
     @Override
     protected void execute() {
-        //
+        Arrays.stream(topics).forEach(topic ->
+                mqttClient.subscribeWith()
+                        .topicFilter(topic)
+                        .callback(mqttMessage -> {
+                            try {
+                                String receivedTopic = mqttMessage.getTopic().toString();
+                                try {
+                                    JsonNode payload = objectMapper.readTree(mqttMessage.getPayloadAsBytes());
+                                    // TODO : log
+                                    addMessage(new MqttDataMessage(receivedTopic, payload));
+                                    addMessage(new StatisticsMessage(getId(),
+                                            StatisticsMessage.StatisticsDataType.OUTPUT));
+                                } catch (JsonParseException e) {
+                                    addMessage(new StatisticsMessage(getId(),
+                                            StatisticsMessage.StatisticsDataType.ERROR,
+                                            e.getMessage()));
+                                }
+                            } catch (Exception e) {
+                                System.err.println("Error processing message: " + e.getMessage());
+                            }
+                        })
+                        .send()
+                        .whenComplete((subAck, throwable) -> {
+                            if (throwable != null) {
+                                // TODO log
+                                addMessage(new StatisticsMessage(getId(), StatisticsMessage.StatisticsDataType.ERROR,
+                                        throwable.getMessage()));
+                            } else {
+                                // TODO log (topic 구독 성공)
+                            }
+                        })
+        );
     }
 
     @Override
     protected void terminate() {
-        try {
-            this.mqttClient.close();
-        } catch (MqttException e) {
-            throw new RuntimeException(e);
-        }
+        this.mqttClient.disconnect();
 
         // 여기서 outputWire도 멈춰줘야함 !!
     }
